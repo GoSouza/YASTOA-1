f957728e057ba17db56ee709322aa196f8583b61
SouzaGo <esx.gosouza@terceiros.mag.com.br>
2025-05-07 10:05:39 -0300

    fix: removendo

--- yastoaRecode/Startup.cs
+++ yastoaRecode/Startup.cs
@@ -27,7 +27,6 @@ namespace yastoaRecode
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
